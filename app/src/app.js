c9f5b4cfd1eec56a97dad2c858b92a6616d7f262
devsecops-pipeline <joytallasaah@gmail.com>
2026-06-10 22:21:49 +0100

    Update app.js

--- app/src/app.js
+++ app/src/app.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const app = express();
 
+const AWS_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE"; // SECRET VOLONTAIREMENT EXPOSÉ POUR TEST
+
+app.get('/', (req, res) => {
+  res.send('API DevSecOps - Projet 12 - Vulnérable intentionnellement');
+});
+
+app.get('/user', (req, res) => {
+  const userId = req.query.id;
+  res.send('User data: ' + userId);
+});
+
+app.listen(3000, () => console.log('App running on port 3000'));
